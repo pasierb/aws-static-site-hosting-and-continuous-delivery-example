3fe0b4dee5df342b68dadc45345e1640039717da
Michal Pasierbski <michal.pasierbski@ef.com>
2020-05-02 19:34:09 +0200

    Remove explicit account and region

--- bin/app-stack.js
+++ bin/app-stack.js
@@ -5,4 +5,4 @@ const { AppStack } = require('../lib/app-stack');
 
 const app = new cdk.App();
 
-new AppStack(app, 'AppStack', { env: { account: '112135394201', region: 'eu-west-1' }});
+new AppStack(app, 'AppStack');
